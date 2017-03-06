ea9feca750d274fa8ed2801761e82f6da62358b7
carloshimself <oficialcarloshimself@gmail.com>
2017-03-06 02:57:34 -0300

    Atualizando Entidades

--- LibraryProject/LibraryProject.Domain/Entities/Customer.cs
+++ LibraryProject/LibraryProject.Domain/Entities/Customer.cs
@@ -4,14 +4,28 @@ namespace LibraryProject.Domain.Entities
 {
     public class Customer
     {
+        public Customer(string firstName, string lastName, DateTime? birthDate, string email, User user)
+        {
+            Id = Guid.NewGuid();
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+            Email = email;
+            User = user;
+
+            //Validações
+        }
+
         public Guid Id { get; private set; }
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
         public DateTime? BirthDate { get; private set; }
-        public bool Active { get; private set; }
-        public string Username { get; private set; }
         public string Email { get; private set; }
-        public string Password { get; private set; }
+        public User User { get; private set; }
 
+        public override string ToString()
+        {
+            return $"{FirstName}{LastName}";
+        }
     }
 }

982b936f3ed8597b8c839eb2cb29d7fcdb6d9e04
carloshimself <oficialcarloshimself@gmail.com>
2017-03-06 02:20:48 -0300

    Inicio do Projeto!

---
+++ LibraryProject/LibraryProject.Domain/Entities/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LibraryProject.Domain.Entities
+{
+    public class Customer
+    {
+        public Guid Id { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public DateTime? BirthDate { get; private set; }
+        public bool Active { get; private set; }
+        public string Username { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+
+    }
+}
